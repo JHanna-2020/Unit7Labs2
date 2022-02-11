9ff4aae386cd8980444626f31d6aa658bede70b7
JHanna-2020 <66224091+JHanna-2020@users.noreply.github.com>
2022-02-11 12:27:22 -0600

    Add files via upload

--- FlowerGarden.java
+++ FlowerGarden.java
@@ -14,66 +14,83 @@ public class  FlowerGarden
 	
 	//write add flower
 	public void addFlower(String userInput){
-        int counter=00;
-       for (int i = 0; i<flowers.size();i++){
-           if(flowers.get(i).equals(userInput)){
+        if(findFlower(userInput) == -1){
+            flowers.add("01 " + userInput);
+        } else{
+            int flowerLocation = findFlower(userInput);
 
-               flowers.add(counter+userInput) ;
-           }
-       }
+            int flowerCount = getCount(userInput);
+            int newCount = flowerCount++;
+            String newString = "";
+            if(newCount<10){
+                 newString = "0"+newCount + " " + userInput;
 
+            }
+            else{
+                 newString = newCount + " " + userInput;
+            }
 
-        flowers.add(userInput);
+            flowers.remove(flowerLocation);
+
+            flowers.add(newString);
+        }
 
     }
 	//write get count
-		public String getCount(String input) {
-            int counter = 00;
-            String result = "";
-            for (int i = 0; i < flowers.size(); i++) {
-                String newString =flowers.get(i).substring(3);
-                if (input.equals(newString)) {
-                    result = flowers.get(i);
-                    counter++;
-                } else {
-                    counter = 1;
-                    return counter + result;
-                }
-                if (counter >= 1) {
-                    return "There are " + counter + " " + input + "s in the flower garden";
-                }
 
-            }
-            return "There are no " + input + "s in the flower garden";
-
+    public boolean inRange(int value){
+        if(value >= 0 && value < flowers.size()){
+            return true;
         }
+        return false;
+    }
+
+    public int getCount(String input) {
+        int flowerLocation = findFlower(input);
+        if(inRange(flowerLocation)){
+            return Integer.parseInt(flowers.get(flowerLocation).substring(0,2));
+        }
+        return 0;
+    }
 
     //write get flower name
-	public String getFlower(int num){
-        return flowers.get(num);
+	public String getFlowerName(int num){
+        if(inRange(num)){
+            return flowers.get(num).substring(3);
+
+        }else{
+            return "out of range";
+        }
     }
 	//write in range
 
 	
 	//write find flower
     public int findFlower(String input){
-        input = input.substring(3);
-        System.out.println(input);
-        for (int i =0; i<flowers.size();i++){
-            if(input.equals(flowers.get(i))){
-                i++;
-
+        for(int i = 0; i < flowers.size(); i++) {
+            if(getFlowerName(i).equals(input)){
+                return i;
             }
-
-
         }
-
-        return flowers.lastIndexOf(input);
-
-
+        return -1;
     }
 
-	//write max flower	
+	//write max flower
+    public String maxFlower(){
+        String maxFlower = getFlowerName(0);
+        int maxCount = getCount(maxFlower);
+
+        for(int i = 0; i < flowers.size(); i++){
+            String currentFlower = getFlowerName(i);
+            int currentVal = getCount(currentFlower);
+            if (currentVal > maxCount){
+                maxCount = currentVal;
+                maxFlower = currentFlower;
+            }
+
+        }
+        return maxFlower;
+    }
 
 
 	//write a toString
--- FlowerGardenRunner.java
+++ FlowerGardenRunner.java
@@ -7,13 +7,39 @@ public class FlowerGardenRunner
 	{
 		FlowerGarden fg = new FlowerGarden();
 		fg.addFlower( "daisy" );
-		fg.addFlower( "daisy" );
 		fg.addFlower( "rose" );
 		fg.addFlower( "poppy" );
 		fg.addFlower( "sunflower" );
-		fg.addFlower( "orchid" );
+		fg.addFlower( "daisy" );
 		System.out.println( fg );
-
+		System.out.println( fg.findFlower( "daisy" ) );
+		System.out.println( fg.findFlower( "orchid" ) );
+		System.out.println( fg.getCount( "daisy" ) );
+		System.out.println( fg.getCount( "orchid" ) );
+		System.out.println( fg.getFlowerName( 0 ) );
+		System.out.println( fg.getFlowerName( 9 ) );
+		System.out.println( fg.maxFlower() );
+		fg.addFlower( "daisy" );
+		fg.addFlower( "daisy" );
+		fg.addFlower( "rose" );
+		fg.addFlower( "poppy" );
+		fg.addFlower( "daisy" );
+		fg.addFlower( "lily" );
+		fg.addFlower( "lily" );
+		fg.addFlower( "gardenia" );
+		fg.addFlower( "rose" );
+		fg.addFlower( "poppy" );
+		fg.addFlower( "gardenia" );
+		fg.addFlower( "daisy" );
+		fg.addFlower( "lily" );
+		System.out.println( fg );
+		System.out.println( fg.findFlower( "lily" ) );
+		System.out.println( fg.findFlower( "orchid" ) );
+		System.out.println( fg.getCount( "daisy" ) );
+		System.out.println( fg.getCount( "orchid" ) );
+		System.out.println( fg.getFlowerName( 3 ) );
+		System.out.println( fg.getFlowerName( 15 ) );
+		System.out.println( fg.maxFlower() );
 
 
 
